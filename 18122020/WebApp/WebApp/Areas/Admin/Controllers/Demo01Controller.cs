01f14fc781c251a80e1e7c6add301188dfa5b487
khoabinnet <khoabinnet@outlook.com.vn>
2020-12-23 20:45:49 +0700

    Code area

---
+++ 18122020/WebApp/WebApp/Areas/Admin/Controllers/Demo01Controller.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Demo01")]
+    public class Demo01Controller : Controller
+    {
+        private readonly ILogger<Demo01Controller> _logger;
+
+        public Demo01Controller(ILogger<Demo01Controller> logger)
+        {
+            //_logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
